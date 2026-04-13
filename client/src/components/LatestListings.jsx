aaa8b61a887606229016342added35f98e0d6d7f
sohel <sohelarfi0@gmail.com>
2026-04-13 11:48:04 +0530

    day-3 added the latestlistings, listingCard, Plans option in the home page

--- client/src/components/LatestListings.jsx
+++ client/src/components/LatestListings.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Title from './Title.jsx'
+import {useSelector} from 'react-redux'
+import ListingCard from './ListingCard.jsx'
 
 const LatestListings = () => {
+  const {listings} = useSelector(state => state.listing)
+
+
   return (
-    <div>
+    <div className='mt-20 mb-8'>
         <Title title="Latest Listings" description="Discover the hottest
-        social profiles availablr right now."/>
+        social profiles available right now."/>
+        <div className='flex flex-col gap-6 px-6'>
+          {listings.slice(0,4).map((listing, index)=>(
+            <div key={index} className='mx-auto w-full max-w-3xl rounded-xl'>
+             <ListingCard listing={listing} />
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { Provider } from 'react-redux'
+import {store} from './app/store.js'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,8 +15,10 @@ if(!PUBLISHABLE_KEY){
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{variables:{colorPrimary: '#4f46e5'}}}>
+    <Provider store={store}>
+      <App />
+    </Provider>
     
-    <App />
    </ClerkProvider>
   </BrowserRouter>,
 )
