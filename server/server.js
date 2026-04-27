5ad4954933e4abd05fe0cfb08f4c3fc5fa891cf4
sohel <sohelarfi0@gmail.com>
2026-04-27 12:41:19 +0530

    solved the serverless problem

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
+// import express from 'express';
+// import 'dotenv/config';
+// import cors from "cors";
+// import { clerkMiddleware } from '@clerk/express';
+// import {serve} from 'inngest/express';
+// import {inngest, functions} from './inngest/index.js';
+
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors())
+// app.use(clerkMiddleware())
+
+
+// app.get("/", (req, res)=>res.send("Server is live"))
+// app.use("/api/inngest", serve({client: inngest, functions}))
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+
+
+
+
 import express from 'express';
 import 'dotenv/config';
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express';
-import {serve} from 'inngest/express';
-import {inngest, functions} from './inngest/index.js';
-
+import { serve } from 'inngest/express';
+import { inngest, functions } from './inngest/index.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(clerkMiddleware());
 
+app.get("/", (req, res) => res.send("Server is live"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get("/", (req, res)=>res.send("Server is live"))
-app.use("/api/inngest", serve({client: inngest, functions}))
+// Run locally but not on Vercel
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+export default app;
