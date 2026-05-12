73780d4390dee860d1a8fef36fc2be09d73c0015
sohel <sohelarfi0@gmail.com>
2026-05-12 09:35:53 +0530

    updated the loading page

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,8 +1,25 @@
+import { Loader2Icon } from 'lucide-react';
 import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Loading = () => {
-  return (
-    <div>Loading</div>
+  const {nextUrl } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(nextUrl){
+      setTimeout(()=>{
+        navigate('/'+ nextUrl)
+      }, 6000)
+    }
+  },[])
+    
+    return (
+    <div className='flex justify-center items-center h-[80vh]'>
+      <Loader2Icon className='animate-spin text-indigo-600 size-7'/>
+      
+    </div>
   )
 }
 
